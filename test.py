9b745050f2302c024db59c78cd3165a08843f7c8
LeonardoHoffmann <143719000+LeonardoHoffmann@users.noreply.github.com>
2025-03-06 11:42:40 -0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,28 @@
+# insere os números
+a1 = input("Digite um número: ")
+a2 = input("Digite outro número: ")
+a3 = input("Digite outro número: ")
+a4 = input("Digite outro número: ")
+a5 = input("Digite outro número: ")
+a6 = input("Digite outro número: ")
+a7 = input("Digite outro número: ")
+a8 = input("Digite outro número: ")
+a9 = input("Digite outro número: ")
+a10 = input("Digite o último número: ")
+# converte os números em int
+n1 = int(a1)
+n2 = int(a2)
+n3 = int(a3)
+n4 = int(a4)
+n5 = int(a5)
+n6 = int(a6)
+n7 = int(a7)
+n8 = int(a8)
+n9 = int(a9)
+n10 = int(a10)
+# soma os números
+b = n1 + n2 + n3 + n4 + n5 + n6 + n7 + n8 + n9 + n10
+# divide-os por 10 para calcular a média
+r = b / 10
+# resultado
+print("A Média de todos os 10 números é de:", r)
